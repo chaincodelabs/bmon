59f11ce42018776a901323c415e1118d61c39aa8
James O'Beirne <james.obeirne@pm.me>
2022-10-04 15:16:02 -0400

    LogProgress migration, docke-compose prod fixes

--- bmon/server_tasks.py
+++ bmon/server_tasks.py
@@ -4,7 +4,6 @@ import datetime
 import django
 from celery import Celery
 from django.conf import settings
-from django.utils import timezone
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmon.settings')
 django.setup()
@@ -27,4 +26,4 @@ def receive_bitcoind_event(event: dict, linehash: str):
 
     models.LogProgress.objects.update_or_create(
         host=instance.host, defaults={
-            'loghash': linehash, 'timestamp': timezone.now()})
+            'loghash': linehash, 'timestamp': datetime.datetime.utcnow()})
