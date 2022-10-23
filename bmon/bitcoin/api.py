2028454b5e67579af79f1a98cc1999002444061a
James O'Beirne <james.obeirne@pm.me>
2022-10-23 11:46:18 -0400

    Warm up bitcoin RPC

--- bmon/bitcoin/api.py
+++ bmon/bitcoin/api.py
@@ -1,3 +1,4 @@
+import time
 import typing as t
 import logging
 from concurrent.futures import ThreadPoolExecutor
@@ -13,17 +14,43 @@ log = logging.getLogger(__name__)
 
 
 @lru_cache
-def get_rpc(hosts: t.Tuple[Host]) -> t.Dict[str, BitcoinRpc]:
-    rpcs = {}
-    for host in hosts:
-        # TODO: assumes that all hosts use same ports, credentials
-        url = (
-            f"http://{settings.BITCOIN_RPC_USER}:"
-            f"{settings.BITCOIN_RPC_PASSWORD}@{host.bmon_ip}"
-            f":{settings.BITCOIN_RPC_PORT}"
-        )
-        rpcs[host.name] = BitcoinRpc(url)
-    return rpcs
+def get_rpc(host: None | str = None, boot_tries: int = 5, boot_delay_secs: int = 5):
+    """
+    Return an RPC object to bitcoind.
+
+    Will block until successfully connected.
+    """
+    host = host or settings.BITCOIN_RPC_HOST
+    url = (
+        f"http://{settings.BITCOIN_RPC_USER}:"
+        f"{settings.BITCOIN_RPC_PASSWORD}@{host}"
+        f":{settings.BITCOIN_RPC_PORT}"
+    )
+    rpc = BitcoinRpc(url)
+
+    if boot_tries == 0:
+        return rpc
+
+    while boot_tries > 0:
+        boot_tries -= 1
+        try:
+            rpc.getblockchaininfo()['chain']
+            return rpc
+        except Exception as e:
+            log.info("hit excption waiting for bitcoin rpc to boot: %s",
+                     e.__class__.__name__)
+            log.debug("bitcoin RPC exception", exc_info=e)
+            time.sleep(boot_delay_secs)
+            boot_delay_secs *= 2
+
+    raise RuntimeError(f"couldn't boot RPC {url}")
+
+
+
+@lru_cache
+def get_rpc_for_hosts(hosts: t.Tuple[Host]) -> t.Dict[str, BitcoinRpc]:
+    # TODO: assumes that all hosts use same ports, credentials
+    return {host.name: get_rpc(host.bmon_ip) for host in hosts}
 
 
 RPC_ERROR_RESULT = object()
@@ -37,7 +64,7 @@ def gather_rpc(rpc_call_arg: str | t.Callable) -> t.Dict[str, t.Any]:
         rpc_call_arg: either a string that represents the RPC call or a
             function that takes the RPC object as its only argument.
     """
-    rpcmap = get_rpc(get_bitcoind_hosts())
+    rpcmap = get_rpc_for_hosts(get_bitcoind_hosts())
     promises = {}
     results = {}
 
