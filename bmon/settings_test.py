ea59da3391d32d473f48005b5b93328f01c0b4dc
James O'Beirne <james.obeirne@pm.me>
2022-10-29 17:16:22 -0400

    test fixes

--- bmon/settings_test.py
+++ bmon/settings_test.py
@@ -9,3 +9,8 @@ DATABASES = {
         'NAME': ':memory:',
     },
 }
+
+REDIS_LOCAL_URL = "redis://redis:6379/11"
+REDIS_SERVER_URL = "redis://redis:6379/10"
+REDIS_HOST = "redis"
+REDIS_LOCAL_HOST = "redis"
