d04768b161fb6449d0bb77c4343edc1b95142b20
James O'Beirne <james.obeirne@pm.me>
2023-04-12 15:23:26 -0400

    disable loki -- basically useless

--- bmon/settings.py
+++ bmon/settings.py
@@ -232,7 +232,7 @@ if SENTRY_DSN:
         # Set traces_sample_rate to 1.0 to capture 100%
         # of transactions for performance monitoring.
         # We recommend adjusting this value in production,
-        traces_sample_rate=1.0,
+        traces_sample_rate=0.4,
 
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
