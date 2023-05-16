b5ae70b27d32fcb852ca8e281075a6d20e1bd377
James O'Beirne <james.obeirne@pm.me>
2023-05-16 16:47:06 -0400

    alert on slow header to tip

--- bmon/bitcoind_tasks.py
+++ bmon/bitcoind_tasks.py
@@ -466,6 +466,12 @@ def process_line(
             util.pushover_notification(
                 f"[{host.name}] saw block download timeout for {got.blockhash}")
 
+        elif isinstance(got, models.HeaderToTipEvent):
+            if got.header_to_tip_secs > 2:
+                util.pushover_notification(
+                    f"[{host.name}] slow header-to-tip "
+                    f"({got.header_to_tip_secs}) for {got.blockhash}")
+
         try:
             got.full_clean()
         except Exception:
