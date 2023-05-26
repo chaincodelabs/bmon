8a3a517fa902328715105c8dc31c5d39861e29f7
James O'Beirne <james.obeirne@pm.me>
2023-05-26 18:53:28 -0400

    web: tips draft

--- bmon/urls.py
+++ bmon/urls.py
@@ -18,11 +18,12 @@ from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
 
-from .views import main
+from .views import main, headertotip
 from .views_api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", api.urls),
+    path("tips", headertotip),
     path("", main),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- bmon/views.py
+++ bmon/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from bmon.models import HeaderToTipEvent
+
 
 def main(request):
     return render(request, 'index.html', {})
+
+
+def headertotip(request):
+    events = HeaderToTipEvent.objects.order_by('-height')[:100]
+    return render(request, 'tips.html', {"events": events})
